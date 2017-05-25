2d608d6c0c41cdd7ccdceb18c170682bd57506ee
Neil Opena <neil1114o@gmail.com>
2017-05-24 20:14:47 -0400

    fixed counter animation

--- js/counter.js
+++ js/counter.js
@@ -2,7 +2,7 @@ $('.count-a').each(function () {
     $(this).prop('Counter',0).animate({
         Counter: $(this).text()
     }, {
-        duration: 2000,
+        duration: 10000,
         easing: 'swing',
         step: function (now) {
             $(this).text(Math.ceil(now));
