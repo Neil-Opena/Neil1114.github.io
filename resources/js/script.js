4d92e13e1f29e7d2ff5d5db42d6988e8baaa06e3
Neil Opena <neil1114o@gmail.com>
2017-06-29 18:44:42 -0400

    added smooth scrolling

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,13 +1,129 @@
 
 //HEADER BUTTONS
-
-$('header .btn-full').mouseenter(function(){
-	$('header .btn-full i').removeClass('fa-square-o',100,'swing');
-	$('header .btn-full i').addClass('fa-check-square-o' ,100, "swing");
+var buttonHeader = $('header .btn-full');
+var buttonIcon = $('header .btn-full i');
+buttonHeader.mouseenter(function(){
+	buttonIcon.removeClass('fa-square-o',100,'swing');
+	buttonIcon.addClass('fa-check-square-o' ,100, "swing");
 });
 
-$('header .btn-full').mouseleave(function(){
-	$('header .btn-full i').addClass('fa-square-o',100,'swing');
-	$('header .btn-full i').removeClass('fa-check-square-o',100,'swing');
+buttonHeader.mouseleave(function(){
+	buttonIcon.addClass('fa-square-o',100,'swing');
+	buttonIcon.removeClass('fa-check-square-o',100,'swing');
 });
 
+buttonHeader.click(function(){
+	$('html, body').animate({scrollTop: $('.section-about-me').offset().top}, 1000);
+});
+
+
+
+//NAV BUTTONS
+$('.js-btn-header').mouseenter(function(){
+	$('.js-header').css({
+		'color':'inherit',
+		'background-color':'#f39c12'
+	});
+});
+$('.js-btn-header').mouseleave(function(){
+	$('.js-header').css({
+		'color':'transparent',
+		'background-color':'transparent'
+	});
+});
+$('.js-btn-about-me').mouseenter(function(){
+	$('.js-about-me').css({
+		'color':'inherit',
+		'background-color':'#f39c12'
+	});
+});
+$('.js-btn-about-me').mouseleave(function(){
+	$('.js-about-me').css({
+		'color':'transparent',
+		'background-color':'transparent'
+	});
+});
+$('.js-btn-education').mouseenter(function(){
+	$('.js-education').css({
+		'color':'inherit',
+		'background-color':'#f39c12'
+	});
+});
+$('.js-btn-education').mouseleave(function(){
+	$('.js-education').css({
+		'color':'transparent',
+		'background-color':'transparent'
+	});
+});
+$('.js-btn-statistics').mouseenter(function(){
+	$('.js-statistics').css({
+		'color':'inherit',
+		'background-color':'#f39c12'
+	});
+});
+$('.js-btn-statistics').mouseleave(function(){
+	$('.js-statistics').css({
+		'color':'transparent',
+		'background-color':'transparent'
+	});
+});
+$('.js-btn-portfolio').mouseenter(function(){
+	$('.js-portfolio').css({
+		'color':'inherit',
+		'background-color':'#f39c12'
+	});
+});
+$('.js-btn-portfolio').mouseleave(function(){
+	$('.js-portfolio').css({
+		'color':'transparent',
+		'background-color':'transparent'
+	});
+});
+$('.js-btn-contact').mouseenter(function(){
+	$('.js-contact').css({
+		'color':'inherit',
+		'background-color':'#f39c12'
+	});
+});
+$('.js-btn-contact').mouseleave(function(){
+	$('.js-contact').css({
+		'color':'transparent',
+		'background-color':'transparent'
+	});
+});
+
+$('a[href*="#"]')
+	  // Remove links that don't actually link to anything
+	  .not('[href="#"]')
+	  .not('[href="#0"]')
+	  .click(function(event) {
+	    // On-page links
+	    if (
+	      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+	      && 
+	      location.hostname == this.hostname
+	    ) {
+	      // Figure out element to scroll to
+	      var target = $(this.hash);
+	      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+	      // Does a scroll target exist?
+	      if (target.length) {
+	        // Only prevent default if animation is actually gonna happen
+	        event.preventDefault();
+	        $('html, body').animate({
+	          scrollTop: target.offset().top
+	        }, 1000, function() {
+	          // Callback after animation
+	          // Must change focus!
+	          var $target = $(target);
+	          $target.focus();
+	          if ($target.is(":focus")) { // Checking if the target was focused
+	            return false;
+	          } else {
+	            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+	            $target.focus(); // Set focus again
+	          };
+	        });
+	      }
+	    }
+	  });
