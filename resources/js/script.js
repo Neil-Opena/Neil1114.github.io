3fa34c4fc743e14eb70916720dd6c2af9d202a8a
Neil Opena <neil1114o@gmail.com>
2017-07-04 22:49:25 -0400

    began work for portfolio

--- resources/js/script.js
+++ resources/js/script.js
@@ -330,32 +330,16 @@ $(window).scroll(function() {
   var oTop = $('#statistics').offset().top - window.innerHeight;
   if (a == 0 && $(window).scrollTop() > oTop) {
   	$('.counter').countTo();
-    // $('.counter').each(function() {
-    //   var $this = $(this),
-    //     countTo = $this.attr('data-count');
-    //   $({
-    //     countNum: $this.text()
-    //   }).animate({
-    //       countNum: countTo
-    //     },
-
-    //     {
-
-    //       duration: 2000,
-    //       easing: 'swing',
-    //       step: function() {
-    //         $this.text(Math.floor(this.countNum));
-    //       },
-    //       complete: function() {
-    //         $this.text(this.countNum);
-    //         //alert('finished');
-    //       }
-
-    //     });
-    // });
     a = 1;
   }
 
+});
+
+//SLICK
+$('.cycle').slick({
+	infinite: true,
+	slidesToShow: 4,
+	slidesToScroll:1,
 });
 
 //ENDING
